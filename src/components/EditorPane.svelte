a6ba40f8c97bb3775ddd2068faca317b1bb7e32e
wizlijun <me@bruce.com>
2026-05-08 12:22:30 +0800

    feat(editor-pane): render ExternalChangeBanner above editor

--- src/components/EditorPane.svelte
+++ src/components/EditorPane.svelte
@@ -4,6 +4,7 @@
   import RichEditor from './RichEditor.svelte'
   import SourceView from './SourceView.svelte'
   import HtmlPreview from './HtmlPreview.svelte'
+  import ExternalChangeBanner from './ExternalChangeBanner.svelte'
 
   let { tab }: { tab: Tab } = $props()
 
@@ -17,20 +18,33 @@
   }
 </script>
 
-{#if tab.mode === 'source'}
-  {#key tab.id}
-    <SourceView value={tab.currentContent} oninput={onSourceInput} />
-  {/key}
-{:else if tab.kind === 'html'}
-  {#key tab.id}
-    <HtmlPreview html={tab.currentContent} />
-  {/key}
-{:else}
-  {#key tab.id}
-    <RichEditor
-      {tab}
-      onFlush={onRichFlush}
-      wrapAsCodeBlock={tab.kind === 'code' ? (tab.language ?? '') : undefined}
-    />
-  {/key}
-{/if}
+<div class="editor-stack">
+  <ExternalChangeBanner {tab} />
+  {#if tab.mode === 'source'}
+    {#key tab.id}
+      <SourceView value={tab.currentContent} oninput={onSourceInput} />
+    {/key}
+  {:else if tab.kind === 'html'}
+    {#key tab.id}
+      <HtmlPreview html={tab.currentContent} />
+    {/key}
+  {:else}
+    {#key tab.id}
+      <RichEditor
+        {tab}
+        onFlush={onRichFlush}
+        wrapAsCodeBlock={tab.kind === 'code' ? (tab.language ?? '') : undefined}
+      />
+    {/key}
+  {/if}
+</div>
+
+<style>
+  .editor-stack {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    min-width: 0;
+    min-height: 0;
+  }
+</style>
